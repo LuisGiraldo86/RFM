ba04e9faf4018a95d500d1d4b919d1689859be09
Luis Giraldo <luisggon@protonmail.com>
2024-05-07 20:47:53 +0200

    test new transformers and grouping

--- tests/testing.py
+++ tests/testing.py
@@ -13,16 +13,36 @@ FOLDER_PATH = '/mnt/287A29DF7A29AA90/PythonProjects/auxiliarData/RFM'
 #fetch_web_data(WEB_PATH, FOLDER_PATH)
 
 df = load_dataset(FOLDER_PATH)
-df
 
-rfm_pipe = Pipeline([
+recency = Pipeline([
     ('recency', Recency(date_to_compare=pd.Timestamp(2013,1,10)).set_output(transform='pandas'))
+])
+spending = Pipeline([
+    ('spending', Spending().set_output(transform='pandas'))
+])
+identity = Pipeline([
+    ('identity', Identity().set_output(transform='pandas'))
 ])
 
 rfm_trans = ColumnTransformer([
-    ('rfm', rfm_pipe, ['Customer ID', 'InvoiceDate'])
+    ('recency', recency, ['Customer ID', 'InvoiceDate']),
+    ('spending', spending, ['Quantity', 'Price']),
+    ('identity', identity, ['Customer ID', 'Invoice'])
 ],
 remainder='drop').set_output(transform='pandas')
 
-rfm_trans.fit_transform(df)
+df_1 = rfm_trans.fit_transform(df)
 
+del df
+
+df_2 = df_1.groupby(by=['identity__Customer ID', 'identity__Invoice'], as_index=False).agg({
+    'recency__recency'  : 'min',
+    'spending__spending': 'sum',
+})
+df_3 = df_2.groupby(by='identity__Customer ID', as_index=False).agg({
+    'identity__Invoice': 'count',
+    'recency__recency': 'min',
+    'spending__spending': 'sum'
+})
+df_3.columns = ['customerID', 'frequency', 'recency', 'money']
+df_3
